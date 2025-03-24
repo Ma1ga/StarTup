714e8903e32fb19e7f2b3ae257ee8817dc9b8ab2
Ma1ga <160391592+Ma1ga@users.noreply.github.com>
2025-03-24 16:21:32 +0200

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const animatedItems = document.querySelectorAll(".animation"); // Находим все элементы с анимацией
+    const animatedItems = document.querySelectorAll(".animation");
 
     function checkVisibility() {
         animatedItems.forEach(item => {
-            const itemPosition = item.getBoundingClientRect().top; // Верхняя граница элемента
-            const screenHeight = window.innerHeight; // Высота экрана
+            const itemPosition = item.getBoundingClientRect().top; 
+            const screenHeight = window.innerHeight; 
 
-            if (itemPosition < screenHeight * 0.8) { // Если элемент попал в зону видимости
+            if (itemPosition < screenHeight * 0.8) { 
                 item.classList.remove("slide-left", "slide-right"); // Убираем классы анимации
                 item.style.transition = "transform 0.8s ease, opacity 0.8s ease"; // Добавляем плавный эффект
                 item.style.transform = "translateX(0) scale(1)"; // Возвращаем в исходное положение
